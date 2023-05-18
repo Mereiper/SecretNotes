732f588ab1cbd7fcdc8e939ae364f8fd292400f2
Emre Iper <mereiper789@gmail.com>
2023-05-18 18:01:59 +0300

    Interface added

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 import tkinter
 import setup
 
+# Screen
 screen = tkinter.Tk()
 setup.setup_screen(screen)
 
+# Image
+image = tkinter.PhotoImage(file='top-secret.png')
+canvas = tkinter.Canvas(height=80, width=80)
+canvas.create_image(50, 50, image=image)
+
+# Labels
+label = tkinter.Label()
+label2 = tkinter.Label()
+label3 = tkinter.Label()
+setup.setup_labels(label=label, label2=label2, label3=label3)
+
+# Texts
+entry = tkinter.Entry()
+entry2 = tkinter.Entry()
+text = tkinter.Text()
+setup.setup_texts(entry=entry, entry2=entry2, text=text)
+
+# Buttons
+button = tkinter.Button()
+button2 = tkinter.Button()
+setup.setup_buttons(button=button, button2=button2)
+
+setup.setup_placement(canvas=canvas, label=label, entry=entry, label2=label2, text=text,
+                      label3=label3, entry2=entry2, button=button, button2=button2)
 screen.mainloop()
--- setup.py
+++ setup.py
@@ -3,3 +3,32 @@ def setup_screen(screen):
     screen.maxsize(300, 500)
     screen.title("Secret Notes")
     screen.config(padx=10, pady=10)
+
+
+def setup_labels(label, label2, label3):
+    label.config(text="Enter your title", font="Arial", pady=10)
+    label2.config(text="Enter your Secret", font="Arial", pady=5)
+    label3.config(text="Enter master key", font="Arial", pady=5)
+
+
+def setup_texts(entry, entry2, text):
+    entry.config(width=30)
+    entry2.config(width=20)
+    text.config(width=30, height=10)
+
+
+def setup_buttons(button, button2):
+    button.config(text="Save & Encrypt")
+    button2.config(text="Decrypt")
+
+
+def setup_placement(canvas, label, entry, label2, text, label3, entry2, button, button2):
+    canvas.pack()
+    label.pack()
+    entry.pack()
+    label2.pack()
+    text.pack()
+    label3.pack()
+    entry2.pack()
+    button.pack()
+    button2.pack()
